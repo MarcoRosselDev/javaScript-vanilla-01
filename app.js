e5a20d5338510f5a3cd7a124d41d2702dd59d790
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-26 21:27:51 -0400

    random number for background color and text

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const color = document.querySelector('.color');
 //detector de eventos para los eventos de clic ->
 
 btn.addEventListener('click', function() {
-    console.log(document.body);
+    //ger random number between 0 - 3
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
