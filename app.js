8e77df4f09c62d2ac64674752211056c392070d6
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-26 21:17:53 -0400

    event listener for the click events

---
+++ app.js
@@ -0,0 +1,10 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+//event listener for the click events
+//detector de eventos para los eventos de clic ->
+
+btn.addEventListener('click', function() {
+    console.log(document.body);
+});
