0763ce975bb7b1c029d81bf025bae316aa2c881c
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-27 12:46:36 -0400

    hex logic random color

---
+++ hex.js
@@ -0,0 +1,10 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+//need something like this #f74920
+//six values random.
+//and the # iths not alterable. 
+btn.addEventListener('click', function() {
+    let hexColor = '#';
+})
